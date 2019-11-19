0b1e7bf064905945528d60c1559a0bd4ec4774a5
Trần Đạt <dattran2723@gmail.com>
2019-11-19 18:07:37 +0700

    start project

---
+++ Services/Implementations/AD/UserService.cs
@@ -0,0 +1,15 @@
+﻿using Abstractions.Services.AD;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Implementations.AD
+{
+    public class UserService : IUserService
+    {
+        public void GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
